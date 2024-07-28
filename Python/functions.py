90f558e517e3a0d3a61a790caa5b134f39bcf4c6
RescuedBuffalo <aidan.kosik@colorado.edu>
2024-07-28 16:59:59 -0700

    add get x, y and shooter id functions

--- Python/functions.py
+++ Python/functions.py
@@ -15,3 +15,30 @@ def convertDuration(row):
         return int(splits[0]) * 60 + int(splits[1])
     else:
         return 0
+    
+def getXCoord(row):
+    '''
+    Function to get the x coordinate of the shot
+    @param row: row of a dataframe
+    '''
+    event_details = json.loads(row['event_details'])
+    return event_details.get('xCoord')
+    
+def getYCoord(row):
+    '''
+    Function to get the y coordinate of the shot
+    @param row: row of a dataframe
+    '''
+    event_details = json.loads(row['event_details'])
+    return event_details.get('yCoord')
+
+def getShootingPlayerId(row):
+    '''
+    Function to get the player id of the player who took the shot
+    @param row: row of a dataframe
+    '''
+    event_details = json.loads(row['event_details'])
+    if row['type_code'] == GOAL:
+        return event_details.get('scoringPlayerId', '')
+    else:
+        return event_details.get('shootingPlayerId', '')
