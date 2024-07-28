835078c0d92e3d5b96efb69a8669e1ec02299382
RescuedBuffalo <aidan.kosik@colorado.edu>
2024-07-28 13:50:41 -0700

    add convert duration to functions

--- Python/functions.py
+++ Python/functions.py
@@ -4,3 +4,14 @@ def createFullName(row):
     @param row: row of a dataframe
     '''
     return row['last_name'] + ', ' + row['first_name']
+
+def convertDuration(row):
+    '''
+    Function to convert duration from MM:SS format to seconds.
+    @param row: row of a dataframe
+    '''
+    if row['duration'] is not None:
+        splits = row['duration'].split(':')
+        return int(splits[0]) * 60 + int(splits[1])
+    else:
+        return 0
